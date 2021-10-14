7749b2c09a729e5cf4d06f5b949bca6e46bd1509
Gosai99nikita <92090072+Gosai99nikita@users.noreply.github.com>
2021-10-14 12:11:30 +0530

    Add files via upload

---
+++ ip-address-tracker-master/config.js
@@ -0,0 +1,4 @@
+var config = {
+    myaccesskey_map : 'pk.eyJ1IjoibmlraXRhNjciLCJhIjoiY2t1bGJqZWFxMTJkbDJxbzY4dXQ3dmw3YiJ9.Yi21Ly7fCpf5v_BNVTvBdg',
+    myaccesskey_ip : 'at_Ni1bnBIXQxTPUyEuoDRlu05rq9b5V&ipAddress=',
+  }
